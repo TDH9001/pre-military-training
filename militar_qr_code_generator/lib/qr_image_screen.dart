8159e39c48fe1623fd5e0768fc7bcc2113d695c3
TDH9001 <mohamedyehia707@gmail.com>
2025-06-02 16:27:15 +0300

    finished the de-cription

--- militar_qr_code_generator/lib/qr_image_screen.dart
+++ militar_qr_code_generator/lib/qr_image_screen.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:encrypt/encrypt.dart' as enc;
 
 class QrImageScreen extends StatelessWidget {
   QrImageScreen({super.key, required this.s});
   final String s;
+  //encryprtion key : 8332767606048159
 
   @override
   Widget build(BuildContext context) {
     List<String> list = s.split("|||");
+    //using ARS symetric encryption
+    var key = enc.Key.fromUtf8("8332767606048159");
+    "8332767606048159";
+    final iV = enc.IV.fromBase16(key.base16);
+    final encrypter = enc.Encrypter(enc.AES(key));
 
+    final encrypted = encrypter.encrypt(s, iv: iV);
+    final decripted = encrypter.decrypt64(encrypted.base64, iv: iV);
     return Scaffold(
       body: Center(
         child: ListView(
@@ -29,6 +38,24 @@ class QrImageScreen extends StatelessWidget {
               child: Text(
                 "برجاء الاحتفاظ بصوره من الكود للطباعه",
                 style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+            ),
+            RepaintBoundary(
+              child: Center(
+                child: QrImageView(
+                  data: encrypted.base64,
+                  version: QrVersions.auto,
+                  size: 300,
+                ),
+              ),
+            ),
+            RepaintBoundary(
+              child: Center(
+                child: QrImageView(
+                  data: decripted,
+                  version: QrVersions.auto,
+                  size: 300,
+                ),
               ),
             ),
 
